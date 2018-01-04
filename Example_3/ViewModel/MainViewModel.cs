c9fc0e5c8724825a35824969137de06fd9a4efa4
Ian <ianburrows.sa@gmail.com>
2018-01-04 16:21:47 +0100

    Adding to details complete

--- Example_3/ViewModel/MainViewModel.cs
+++ Example_3/ViewModel/MainViewModel.cs
@@ -2,6 +2,7 @@ using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.CommandWpf;
 using System;
 using System.Collections.ObjectModel;
+using System.Threading;
 
 namespace Example_3.ViewModel
 {
@@ -10,31 +11,100 @@ namespace Example_3.ViewModel
     {
         #region Buttons
         public RelayCommand StartGenerating { get; set; }
-        public RelayCommand ToDetails { get; set; }
+        public RelayCommand<CargoVM> ToDetails { get; set; }
+        //public RelayCommand<CargoItemVM> ToDetails { get; set; }
         public RelayCommand Clear { get; set; }
         #endregion
 
         #region collection
         public ObservableCollection<CargoVM> Cargo { get; set; }
+        public ObservableCollection<CargoVM> ReadyCollection { get; set; }
+        public ObservableCollection<CargoItemVM> DetailsCollection { get; set; }
+        //public ObservableCollection<CargoItemVM> DetailsCollection { get; set; }
         #endregion
+
+        #region properties for waiting for
+        public string Description { get; set; }
+        public int DeliveryTime { get; set; }
+        public ObservableCollection<CargoItemVM> CargoItem{ get; set; }
+        #endregion
+
+        #region properties for details
+        public string Name { get; set; }
+        public string Weight { get; set; }
+        public string Amount { get; set; }
+
+        #endregion
+
+        Thread TimerThread;
+        Random random;
+        Timer ArrayGenerationTimer;
+
+        public CargoVM SelectedItem { get; set; }
+
+        public CargoVM[] CargoArray { get; set; }
 
         public MainViewModel()
         {
             Cargo = new ObservableCollection<CargoVM>();
-            DemoData();
 
-            StartGenerating = new RelayCommand(() => { });
-            ToDetails = new RelayCommand(() => { });
-            Clear = new RelayCommand(() => { });
+            DetailsCollection = new ObservableCollection<CargoItemVM>();
+            ReadyCollection = new ObservableCollection<CargoVM>();
+            StartGenerating = new RelayCommand(() => { StartTimer(); });
 
+            ToDetails = new RelayCommand<CargoVM>((p) => 
+            {
+                foreach (var item in p.CargoItem)
+                {
+                    DetailsCollection.Add(item);
+                }
+                
+            });
+            Clear = new RelayCommand(() => 
+            {
+                if (DetailsCollection == null)
+                {
+                    DetailsCollection = new ObservableCollection<CargoItemVM>();
+                }
+
+                DetailsCollection.Clear();
+            });
+
+            GenerateArrayData();
+            
+
+        }
+
+        private void StartTimer()
+        {
+            TimerThread = new Thread(new ThreadStart(StartArrayTimer));
+            TimerThread.IsBackground = true;
+            TimerThread.Start();
             
         }
 
-        private void DemoData()
+        private void StartArrayTimer()
         {
-            CargoVM[] CargoArray = new CargoVM[5];
+            ArrayGenerationTimer = new Timer(StartRandomArrayGeneration, null, 0, 1000);
+
+        }
+
+        private void StartRandomArrayGeneration(object state)
+        {
+            random = new Random();
+            int randomNumber = random.Next(0, 4);
+            App.Current.Dispatcher.Invoke(()=> {
+                ReadyCollection.Add(CargoArray[randomNumber]);
+                Cargo.Add(CargoArray[randomNumber]);
+            });
+            
+        }
+
+        private void GenerateArrayData()
+        {
+            CargoArray = new CargoVM[5];
             CargoArray[0] = new CargoVM("Vienna", 5, new CargoItemVM(5,6,"Bricks"));
-            CargoArray[0].AddItem(new CargoItemVM(8,9,"Timber"));
+            CargoArray[0].AddItem(new CargoItemVM(8, 9,"Timber"));
             CargoArray[0].AddItem(new CargoItemVM(8, 9, "Nails"));
             CargoArray[0].AddItem(new CargoItemVM(8, 9, "Hammer"));
 
@@ -57,6 +127,7 @@ namespace Example_3.ViewModel
             CargoArray[4].AddItem(new CargoItemVM(8, 9, "Timber"));
             CargoArray[4].AddItem(new CargoItemVM(8, 9, "Nails"));
             CargoArray[4].AddItem(new CargoItemVM(8, 9, "Hammer"));
+
         }
     }
 }
